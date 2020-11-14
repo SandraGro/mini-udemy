eca749eca4f01997c87746f6cb1d44c1436e4c8a
SandraGro <sandra.guerreroflores@gmail.com>
2020-11-13 21:44:11 -0600

    Add overlay description to courses

--- src/Components/FeaturedCourses/Courses/Courses.js
+++ src/Components/FeaturedCourses/Courses/Courses.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
-import { Card, Badge, CardDeck } from "react-bootstrap";
+import {
+  Card,
+  Badge,
+  CardDeck,
+  OverlayTrigger,
+  Popover,
+} from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./Courses.scss";
 
@@ -9,22 +15,35 @@ class Courses extends Component {
       <>
         <CardDeck>
           {this.props.featuredCourses.map((course, index) => (
-            <Card className="course-card" key={`cardDeck-${index}`}>
-              <Link to={`/course/${course.slug}`}>
-                <Card.Img variant="top" src={course.thumbnail} />
-                <Card.Body>
-                  <Card.Title>{course.title}</Card.Title>
-                  <Card.Text>{course.author}</Card.Text>
-                  <span>${course.discountedPrice} </span>
-                  <span>${course.originalPrice}</span>
-                  {course.bestseller ? (
-                    <Badge variant="warning">Bestseller</Badge>
-                  ) : (
-                    ""
-                  )}
-                </Card.Body>
-              </Link>
-            </Card>
+            <OverlayTrigger
+              key={`cardDeck-${index}`}
+              placement="right"
+              overlay={
+                <Popover id={`tooltip-${index}`}>
+                <Popover.Title as="h3">Popover bottom</Popover.Title>
+                <Popover.Content>
+                  <strong>Holy guacamole!</strong> Check this info.
+                </Popover.Content>
+              </Popover>
+              }
+            >
+              <Card className="course-card">
+                <Link to={`/course/${course.slug}`}>
+                  <Card.Img variant="top" src={course.thumbnail} />
+                  <Card.Body>
+                    <Card.Title>{course.title}</Card.Title>
+                    <Card.Text>{course.author}</Card.Text>
+                    <span>${course.discountedPrice} </span>
+                    <span>${course.originalPrice}</span>
+                    {course.bestseller ? (
+                      <Badge variant="warning">Bestseller</Badge>
+                    ) : (
+                      ""
+                    )}
+                  </Card.Body>
+                </Link>
+              </Card>
+            </OverlayTrigger>
           ))}
         </CardDeck>
       </>
